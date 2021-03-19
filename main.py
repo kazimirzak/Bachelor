881fad721425de049ad858aa6d4d9d86423ecb65
Kenny <kazimirzak15@gmail.com>
2021-03-19 09:24:07 +0100

    sentiment analysis done

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ import re
 from dateutil import parser
 import gzip
 import json
+import time
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+
 DATA_PATH = "E:\\release"
 
 db = mysql.connector.connect(
-    host="localhost",
     user="root",
-    password="secret",
+    password="",
     database="bachelor"
 )
 cursor = db.cursor()
 
+vader = SentimentIntensityAnalyzer()
+
 
 # Inserts articles into the database using the DATA_PATH. Structure of data should be as described here: http://sciride.org/news.html#datacontent
 def insert_articles():
-    insert_query = "INSERT INTO articles (outlet, date, articleId, title, description, link) VALUES (%s, %s, %s, %s, %s, %s)"
+    insert_query = "INSERT INTO articles (outlet, date, title, description, link) VALUES (%s, %s, %s, %s, %s)"
     for path, dirs, files in os.walk(DATA_PATH):
         for f in fnmatch.filter(files, '*.gz'):
             abs_path = os.path.abspath(os.path.join(path, f))
@@ -28,21 +33,69 @@ def insert_articles():
             with gzip.open(abs_path) as zip:
                 content = json.loads(zip.read())
                 articles = []
-                for articleId, article in content.items():
+                for article_id, article in content.items():
                     title = article['title'].strip()
                     description = article['description'].strip()
                     link = next(iter(article['link'])).strip()
                     if title and description:
-                        articles.append((outlet, date, articleId.strip(), title, description, link))
+                        articles.append((outlet, date, title, description, link))
                 try:
                     cursor.executemany(insert_query, articles)
                     db.commit()
                 except:
-                    print(outlet, date, articleId, title, description, link, sep='\n')
+                    print(outlet, date, title, description, link, sep='\n')
+
+
+def sentiment_analysis():
+    insert_vader = "INSERT INTO vader (articleId, neg, neu, pos, compound) VALUES (%s, %s, %s, %s, %s)"
+
+    insert_textblob = "INSERT INTO textblob (articleId, polarity, subjectivity)" \
+                      "VALUES (%s, %s, %s)"
+    limit = 5000
+    cursor.execute("SELECT MAX(id) FROM articles")
+    num_of_articles = (cursor.fetchone())[0]
+    cursor.execute("SELECT MAX(id) FROM vader")
+    start_offset = (cursor.fetchone())[0]
+    if not start_offset:
+        start_offset = 0
+    for i in range(start_offset, num_of_articles, limit):
+        vs = []
+        ts = []
+        cursor.execute(f"SELECT id, title, description FROM articles WHERE id > {i} ORDER BY id ASC LIMIT {limit}")
+        articles = cursor.fetchall()
+        for article in articles:
+            id = article[0]
+            title = str(article[1])
+            description = str(article[2])
+            vs.append(vader_sentiment(id, f"{title} {description}"))
+            ts.append(textblob_sentiment(id, f"{title} {description}"))
+        cursor.executemany(insert_vader, vs)
+        db.commit()
+        cursor.executemany(insert_textblob, ts)
+        db.commit()
+        print(f"Processed Chunk: {i / limit}/{num_of_articles / limit}!")
+
+
+def vader_sentiment(id, text):
+    sentiment = vader.polarity_scores(text)
+    # return a tuple with:
+    # (articleId, neg, neu, pos, compound).
+    return (id, sentiment['neg'], sentiment['neu'], sentiment['pos'], sentiment['compound'])
+
+
+def textblob_sentiment(id, text):
+    sentiment = TextBlob(text).sentiment
+    # Returns a tuple with:
+    # (articleId, polarity, subjectivity)
+    return (id, sentiment.polarity, sentiment.subjectivity)
 
 
 if __name__ == '__main__':
-    insert_articles()
+    print("Lets fuck shit up!")
+    start_time = time.time()
+    #insert_articles()
+    sentiment_analysis()
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
